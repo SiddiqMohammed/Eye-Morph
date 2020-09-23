37e6701d5441c213469b0ec0028004b33dcbe50a
Siddiq Mohammed <msm2029@gmail.com>
2020-09-23 19:29:28 +0400

    inverse video addition and new video auto updates

--- EyeProject/Assets/test.cs
+++ EyeProject/Assets/test.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -8,63 +9,82 @@ using UnityEngine.Video;
 
 public class test : MonoBehaviour
 {
-    public GameObject[] objects;
-    public RenderTexture[] textures;
-    public VideoClip[] clips;
-    float changer = 5f;
-    string[] files;
-    int clipMonitor = 0;
-    int oldCount;
+	public GameObject[] objects;
+	public RenderTexture[] textures;
+	public VideoClip[] clips;
+	float changer = 5f;
+	string[] files;
+	int clipMonitor = 0;
+	int oldCount;
 
-    private void Start()
-    {
+	private void Start()
+	{
+		scanClips();
+	   
+		oldCount = files.Length;
+		print(oldCount);
 
-        scanClips();
-        oldCount = files.Length;
-        for(int i=0; i<objects.Length; i++)
-        {
-            objects[i].GetComponent<VideoPlayer>().url = files[i];
-        }
-        
-        
-    }
+		if (files.Length != 0)
+		{
+			updateVideo();
+		}
+	}
 
-    
+	void updateVideo()
+	{
+		print(objects.Length);
+		print(files.Length);
 
-    private void Update()
-    {
-        changer -= Time.deltaTime;
-        if (changer < 0)
-        {
-            scanClips();
-            clipMonitor = files.Length - oldCount;
-            if (clipMonitor > 0)
-            {
-                for (int i = 0; i < clipMonitor-1; i++)
-                {
-                    objects[i].GetComponent<VideoPlayer>().url = files[oldCount+i];
-                }
+		print(files);
+		print(string.Join("\n", files));
 
-                oldCount = files.Length;
-            }
-            changer = 5f;
-        }
-    }
+		Array.Reverse(files);
 
-    void scanClips()
-    {
-        files = Directory.GetFiles(Application.dataPath + "/Videos", "*.mp4");
-        
-    }
-    void assignVideos()
-    {
-        for (int i = 0; i < textures.Length; i++)
-        {
-            objects[i].GetComponent<VideoPlayer>().clip = clips[i];
-            objects[i].GetComponent<VideoPlayer>().targetTexture = textures[i];
-        }
-    }
+		for (int i = 0; i < files.Length; i++)
+		{
+			objects[i].GetComponent<VideoPlayer>().url = files[i];
+		}
 
-    
+	}
 
+	void OnApplicationQuit()
+	{
+		for(int i=0; i<files.Length; i++)
+		{
+			textures[i].Release();
+		}
+	}
+
+	private void Update()
+	{
+		changer -= Time.deltaTime;
+		if (changer < 0)
+		{
+			scanClips();
+	
+			clipMonitor = files.Length - oldCount;
+			
+			if (clipMonitor > 0)
+			{
+				updateVideo();
+
+				oldCount = files.Length;
+			}
+			changer = 5f;
+		}
+	}
+
+	void scanClips()
+	{
+		files = Directory.GetFiles(Application.dataPath + "/Videos", "*.mp4");
+		
+	}
+	void assignVideos()
+	{
+		for (int i = 0; i < textures.Length; i++)
+		{
+			objects[i].GetComponent<VideoPlayer>().clip = clips[i];
+			objects[i].GetComponent<VideoPlayer>().targetTexture = textures[i];
+		}
+	}
 }
