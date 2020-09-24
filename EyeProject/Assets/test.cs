9ea6ba2b3d49dd52cf8dddc3e192d3ec51628326
Siddiq Mohammed <msm2029@gmail.com>
2020-09-24 15:33:58 +0400

    test

--- EyeProject/Assets/test.cs
+++ EyeProject/Assets/test.cs
@@ -10,8 +10,11 @@ using UnityEngine.Video;
 
 public class test : MonoBehaviour
 {
-	public GameObject[] objects;
-	public RenderTexture[] textures;
+	List<GameObject> objects= new List<GameObject>();
+	List<GameObject> videoPlayers = new List<GameObject>();
+	public List<RenderTexture> Textures = new List<RenderTexture>();
+	public GameObject parent;
+	// public RenderTexture[] textures;
 	public VideoClip[] clips;
 	float changer = 5f;
 	/*string[] files;*/
@@ -19,48 +22,143 @@ public class test : MonoBehaviour
 	int clipMonitor = 0;
 	int oldCount;
 
+	float width;
+    float height;
+    public int row = 2;
+    public int column = 3;
+
 	private void Start()
 	{
+        createImages();
+		ResponsiveScreen();
+		createVideoPlayer();
+
 		scanClips();
 
 		oldCount = files.Count;
 		/*print(oldCount);*/
 
-		if (files.Count != 0)
-		{
-			updateVideo();
-		}
+		// if (files.Count != 0)
+		// {
+		// 	updateVideo();
+		// }
+		// for (int i = 0; i < 3; i++)
+		// {
+		// 	videoPlayers[i].GetComponent<VideoPlayer>().url = files[i];
+		// }
+
 	}
 
 	void updateVideo()
 	{
-		print(objects.Length);
-		print(files.Count);
+		// print(objects.Count);
+		// print(files.Count);
 
 
 		files.Reverse();
 
-		if (files.Any())
-		{
-			if (files.Count > objects.Length)
-			{
-				files.RemoveAt(files.Count - 1);
-			}
-		}
+		// if (files.Any())
+		// {
+		// 	if (files.Count > videoPlayers.Count)
+		// 	{
+		// 		files.RemoveAt(files.Count - 1);
+		// 	}
+		// }
+		// print(Textures.Count);
+		// print(files.Count);
 
-		for (int i = 0; i < files.Count; i++)
-		{
-			objects[i].GetComponent<VideoPlayer>().url = files[i];
-		}
+		// for (int i = 0; i < files.Count; i++)
+		// {
+			// // objects[i].GetComponent<VideoPlayer>().url = files[i];
+			// GameObject go = new GameObject();
+
+			// // go.AddComponent<RawImage>();
+			// go.AddComponent<VideoPlayer>();
+
+			// go.name = objects.Count.ToString();
+			// objects.Add(go);
+		// createVideoPlayer();
+		// 	videoPlayers[i].GetComponent<VideoPlayer>().url = files[i];
+		// 	videoPlayers[i].GetComponent<VideoPlayer>().Play();
+
+		// }
+
 
 	}
 
+   void createImages()
+    {
+		int x = row * column;
+
+		for (int i = 0; i < x; i++)
+		{
+			GameObject go = new GameObject();
+
+			go.AddComponent<RawImage>();
+			// go.AddComponent<VideoPlayer>();
+
+			go.name = objects.Count.ToString();
+			objects.Add(go);
+		}
+    }
+
+	void ResponsiveScreen()
+    {
+        width = Screen.width / column;
+        height = Screen.height / row;
+		int count = 0;
+        for (int i = 0; i < column; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+
+                // GameObject go = new GameObject();
+                // go.AddComponent<RawImage>();
+				// objects
+                objects[count].GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+                objects[count].GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+                objects[count].GetComponent<Transform>().position = new Vector3(i * width, j * height, 0);
+                objects[count].transform.parent = parent.transform;
+                // go.name = objects.Count.ToString();
+                // objects.Add(go);
+				count += 1;
+
+			}
+		}
+	}
+
+	void createVideoPlayer()
+    {
+        for(int i=0; i<3; i++)
+        
+        {
+            GameObject vp = new GameObject();
+            vp.transform.parent = GameObject.Find("Player").transform;
+            vp.AddComponent<VideoPlayer>();
+
+            vp.GetComponent<VideoPlayer>().playOnAwake = true;
+            vp.GetComponent<VideoPlayer>().isLooping = true;
+            vp.GetComponent<VideoPlayer>().renderMode = VideoRenderMode.RenderTexture;
+            vp.GetComponent<VideoPlayer>().aspectRatio = VideoAspectRatio.Stretch;
+            vp.GetComponent<VideoPlayer>().playbackSpeed = 0.5f;
+            vp.GetComponent<VideoPlayer>().targetTexture = Textures[i];
+            vp.GetComponent<VideoPlayer>().name = videoPlayers.Count.ToString();
+            vp.GetComponent<VideoPlayer>().url = files[i];
+			
+            
+            videoPlayers.Add(vp);
+
+        }
+        
+    }
+
 	void OnApplicationQuit()
 	{
-		for (int i = 0; i < files.Count; i++)
-		{
-			textures[i].Release();
-		}
+		// for (int i = 0; i < files.Count; i++)
+		// {
+		// 	Textures[i].Release();
+		// }
+		print("QUIT");
 	}
 
 	private void Update()
@@ -91,12 +189,12 @@ public class test : MonoBehaviour
 		}
 
 	}
-	void assignVideos()
-	{
-		for (int i = 0; i < textures.Length; i++)
-		{
-			objects[i].GetComponent<VideoPlayer>().clip = clips[i];
-			objects[i].GetComponent<VideoPlayer>().targetTexture = textures[i];
-		}
-	}
+	// void assignVideos()
+	// {
+	// 	for (int i = 0; i < textures.Length; i++)
+	// 	{
+	// 		objects[i].GetComponent<VideoPlayer>().clip = clips[i];
+	// 		objects[i].GetComponent<VideoPlayer>().targetTexture = textures[i];
+	// 	}
+	// }
 }
